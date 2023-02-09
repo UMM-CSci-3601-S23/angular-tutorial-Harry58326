bab0898b91bc626afe725b740fae6efdb39e07f0
Harry <lv000013@morris.umn.edu>
2023-02-09 13:37:15 -0600

    Added a button called Notify Me when a product is over $700

--- src/app/product-alerts/product-alerts.component.spec.ts
+++ src/app/product-alerts/product-alerts.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
 import { ProductAlertsComponent } from './product-alerts.component';
 
 describe('ProductAlertsComponent', () => {
