2ae361e3d0b743567a21c727861dcd0cf26509d5
Harry <lv000013@morris.umn.edu>
2023-02-09 20:30:32 -0600

    Imported the CartService and defined the items property

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class CartComponent {
 
+  items = this.cartService.getItems();
 
+  constructor(
+    private cartService: CartService
+  ) { }
 }
